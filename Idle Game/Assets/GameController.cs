470281f0911d0b4ca8a625905e6506004f8f0cdd
luislombardis <anguilaelectrica3@gmail.com>
2019-04-24 20:58:46 +0200

    Added game controller

---
+++ Idle Game/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text Coins;
+    public Slider GameProgress;
+
+    public long coins;
+    public float progress;
+    private float max = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = 0;
+        progress = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Coins.text = "Coins: " + coins;
+        actualizarSlider(max, progress);
+    }
+
+    private void actualizarSlider(float max, float progress)
+    {
+        float porcentaje;
+        porcentaje = progress / max;
+        GameProgress.value = porcentaje;
+    }
+}
