3ab13307b617b5bc242936732ee47f0e22364843
CoFlavioS <jaguaru40@yahoo.es>
2019-04-27 16:01:20 +0200

    Menu de tienda completo sin enlaces a codigo

---
+++ Idle Game/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    private bool status;
+    public GameObject Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = false;
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenShop()
+    {
+        if(!status)
+        {
+            Menu.SetActive(true);
+            status = true;
+        }
+        else
+        {
+            Menu.SetActive(false);
+            status = false;
+        }
+    }
+}
