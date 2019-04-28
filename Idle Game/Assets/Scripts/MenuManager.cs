4bf545b6dc0b1314b56e0a84e5eb7677f73c44b8
CoFlavioS <jaguaru40@yahoo.es>
2019-04-28 18:02:08 +0200

    Scene saved and coins acounted for

--- Idle Game/Assets/Scripts/MenuManager.cs
+++ Idle Game/Assets/Scripts/MenuManager.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
     private bool status;
     public GameObject Menu;
+    public GameController controller;
+    public TextMeshProUGUI coinsText;
 
     // Start is called before the first frame update
     void Start()
     {
         status = false;
         Menu.SetActive(false);
+        controller = GameObject.Find("GameController").GetComponent<GameController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        coinsText.text = "Coins: " + controller.coins;
     }
 
     public void OpenShop()
