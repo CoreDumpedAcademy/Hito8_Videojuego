fafd52802272e0c3a67997524cb9359f7d0020db
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-04-30 19:03:07 +0200

    Data moved to scriptable objects and object creation script

---
+++ Idle Game/Assets/Scripts/DevData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dev", menuName = "Developer")]
+
+public class DevData : ScriptableObject
+{
+    public Sprite artwork;
+
+    public string devName;
+    public int cost;
+    public float production;
+    public int frequency;
+    public int maxExp;
+}
