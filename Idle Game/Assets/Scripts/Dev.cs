485f59dd2b76fd77786d6629141a8b2543c35df4
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-05-05 19:56:59 +0200

    fixed sloppy fix of sloppy merge

--- Idle Game/Assets/Scripts/Dev.cs
+++ Idle Game/Assets/Scripts/Dev.cs
@@ -43,9 +43,8 @@ public class Dev : MonoBehaviour
     float counter = 0;                    //counts time to know when to produce
     private float increaseExp = 1.15f;
     private float textExp;
+    private string lvlString;
     private float increaseProd = 1.3f;
-
-
 
     void Start()
     {
