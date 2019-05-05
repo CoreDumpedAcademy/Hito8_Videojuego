43f0524db533f0a5bd56b20de7bf058d8f4f7709
CoFlavioS <jaguaru40@yahoo.es>
2019-05-05 20:59:35 +0200

    Pause menu

---
+++ Idle Game/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public bool status;
+    public GameObject Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = false;
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Guardar ()
+    {
+        Debug.Log("Guardando partida");
+    }
+
+    public void SalirGuardar()
+    {
+        Guardar();
+        SceneManager.LoadScene("InitialScene");
+    }
+
+    public void OpenOptions()
+    {
+        if (!status)
+        {
+            Menu.SetActive(true);
+            status = true;
+        }
+        else
+        {
+            Menu.SetActive(false);
+            status = false;
+        }
+    }
+}
