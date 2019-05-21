4484c204c50fa32cf946be916035e0f6ecee7a1b
CoFlavioS <jaguaru40@yahoo.es>
2019-05-21 19:38:39 +0200

    Base buff data structure

---
+++ Idle Game/Assets/Scripts/BuffData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBuff", menuName = "Buffer")]
+
+public class BuffData : ScriptableObject
+{
+    public Sprite artwork;
+
+    public string buffName;
+    public bufftype type;
+    public float addValue;
+
+    public enum bufftype
+    {
+        none,
+        initCoinIncr,
+        prodIncr,
+        rewIncr,
+        expIncr
+    }
+}
