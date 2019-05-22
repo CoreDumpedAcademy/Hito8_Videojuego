3f7f3126b8d1bbbe3be52f1a8765dd99866a244b
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-05-22 14:16:13 +0200

    Loads buffs. Functions that gets a random reset buff

---
+++ Idle Game/Assets/Scripts/BuffController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffController : MonoBehaviour
+{
+    string resetBuffsPath = "BuffTypes";
+    BuffData[] resetBuffs;
+    int resetBuffNumer;
+
+    void Start()
+    {
+        resetBuffs = Resources.LoadAll<BuffData>(resetBuffsPath);
+        resetBuffNumer = resetBuffs.Length;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) )
+        {
+            Debug.Log( getRamdomResetBuff().name );
+        }
+    }
+    public BuffData getRamdomResetBuff()
+    {
+        BuffData buff = resetBuffs[0];    //Initiated to default Buff
+        int buffInd = Random.Range(1, resetBuffNumer);
+        buff = resetBuffs[buffInd];
+        return buff;
+    }
+
+
+    public struct buffValues
+    {
+        int rewardCoinIncr;
+        int initCoinIncr;
+        float prodIncr;
+        float expIncr;
+    }
+}
